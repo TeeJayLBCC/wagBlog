49cf41c39976e3c394c59a1b5a619b8cd1a80cf0
TeeJayLBCC <135385931+TeeJayLBCC@users.noreply.github.com>
2025-02-13 22:12:00 -0800

    Initial commit of local files

---
+++ mysite/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-&gyee&5gekskre$r1a#j#v!r9r0^_po7dbz!m7cfh&xa6vz*%!"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
